0c59aa8a6fe1e46336f5cca309eae20ec8ac4e70
Tam Nguyen <nguyenngoctamit88@gmail.com>
2021-02-27 00:16:23 +0700

    Update regex pattern

--- app/src/main/java/com/tamnn/vnexpressfeeds/util/RegexUtil.kt
+++ app/src/main/java/com/tamnn/vnexpressfeeds/util/RegexUtil.kt
@@ -3,13 +3,13 @@ package com.tamnn.vnexpressfeeds.util
 import java.util.regex.Pattern
 
 object RegexUtil {
-    private const val IMG_PATTERN = "<img[^>]+src=\"([^\">]+)\""
+    private const val IMG_PATTERN = "<img [^>]*src=\"([^\"]+)\"[^>]*>"
     private val imgPattern = Pattern.compile(IMG_PATTERN)
 
     fun getImageUrl(html: String): String? {
         val matcher = imgPattern.matcher(html)
         if(matcher.find()){
-            return matcher.group()
+            return matcher.group(1)
         }
         return null
     }
